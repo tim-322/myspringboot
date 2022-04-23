f00a21854a153ac1fb336ef866968d9903efe7f0
Mind <2994101331@qq.com>
2022-04-23 13:13:06 +0800

    黑马增删改查案例V1.1

--- src/main/java/com/bookmanage/boot/Application.java
+++ src/main/java/com/bookmanage/boot/Application.java
@@ -12,10 +12,11 @@ public class Application {
     public static void main(String[] args) {
         /*比配置文件等级更高的设定，就会覆盖配置文件的配置*/
         //让热部署失效
-//        System.setProperty("spring.devtools.restart.enabled","false");
+        // System.setProperty("spring.devtools.restart.enabled","false");
         ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
         ServiceConfig serviceConfig = run.getBean(ServiceConfig.class);
         System.out.println(serviceConfig);
+        //V1.1
     }
 
 }
